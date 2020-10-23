166f17720c10a18aff2322356be98b8e6f9de5c7
Mindaugas <strumke@yahoo.com>
2020-10-23 11:15:23 +0200

    Updates

---
+++ precache-manifest.de52061b976fb2a007ed7c718aafd99b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68025411bcab9775c84769f4169a306b",
+    "url": "/countrypedia/index.html"
+  },
+  {
+    "revision": "bdf55fdcd817a10ea50d",
+    "url": "/countrypedia/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "591d34b3575efd9c5f6b",
+    "url": "/countrypedia/static/js/2.adc2fc33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/countrypedia/static/js/2.adc2fc33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdf55fdcd817a10ea50d",
+    "url": "/countrypedia/static/js/main.fe6ef0f4.chunk.js"
+  },
+  {
+    "revision": "bf67884212a7dc6a2e99",
+    "url": "/countrypedia/static/js/runtime-main.f586e0e6.js"
+  }
+]);
